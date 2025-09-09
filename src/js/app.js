af0b9af673784a05201b1cc5d1f035e2cdc46134
Kate Crowley <118118942+elalince@users.noreply.github.com>
2025-09-09 12:18:25 +0000

    start

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,3 @@
 import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.showNav();
-flsFunctions.headerBg();
-flsFunctions.showDropdown();
-flsFunctions.mainSlider();
-flsFunctions.instructionReveal();
-flsFunctions.faqOpening();
-flsFunctions.dropdown();
-flsFunctions.callLink();
 // flsFunctions.topBlockScroll();
